af02a6246bf90eb067e1d5834965abeb58d694f9
Svetlin Ankov <svetlin.ankov@live.com>
2016-04-29 18:51:52 +0300

    Writers.CollectionSerializer: Handle Stack<> collections correctly

--- Albite.Serialization/Source/Internal/Writers/Collections/CollectionSerializer.cs
+++ Albite.Serialization/Source/Internal/Writers/Collections/CollectionSerializer.cs
@@ -8,7 +8,14 @@ namespace Albite.Serialization.Internal.Writers.Collections
 {
     internal class CollectionSerializer : ObjectSerializer
     {
-        protected CollectionSerializer() { }
+        private readonly bool _reverse;
+
+        protected CollectionSerializer() : this(false) { }
+
+        protected CollectionSerializer(bool reverse)
+        {
+            _reverse = reverse;
+        }
 
         protected ISerializer _nested;
 
@@ -26,6 +33,12 @@ namespace Albite.Serialization.Internal.Writers.Collections
             // Now write the size
             context.Writer.Write(collection.Count());
 
+            // Needed for Stack.
+            if (_reverse)
+            {
+                collection = collection.Reverse();
+            }
+
             // And finally write all the elements
             foreach (var element in collection)
             {
@@ -38,11 +51,13 @@ namespace Albite.Serialization.Internal.Writers.Collections
         {
             private readonly SerializedType _stype;
             private readonly Type _type;
+            private readonly bool _reverse;
 
-            public D(SerializedType stype, Type type)
+            public D(SerializedType stype, Type type, bool reverse = false)
             {
                 _stype = stype;
                 _type = type;
+                _reverse = reverse;
             }
 
             public bool IsTypeSupported(Type type)
@@ -58,14 +73,14 @@ namespace Albite.Serialization.Internal.Writers.Collections
 
             public virtual IInitiailzableSerializer Create()
             {
-                return new CollectionSerializer();
+                return new CollectionSerializer(_reverse);
             }
         }
 
         public static readonly IDescriptor LinkedListDescriptor = new D(SerializedType.LinkedList, typeof(LinkedList<>));
         public static readonly IDescriptor ListDescriptor = new D(SerializedType.List, typeof(List<>));
         public static readonly IDescriptor QueueDescriptor = new D(SerializedType.Queue, typeof(Queue<>));
-        public static readonly IDescriptor StackDescriptor = new D(SerializedType.Stack, typeof(Stack<>));
+        public static readonly IDescriptor StackDescriptor = new D(SerializedType.Stack, typeof(Stack<>), true);
         public static readonly IDescriptor HashSetDescriptor = new D(SerializedType.HashSet, typeof(HashSet<>));
         public static readonly IDescriptor SortedSetDescriptor = new D(SerializedType.SortedSet, typeof(SortedSet<>));
         #endregion
